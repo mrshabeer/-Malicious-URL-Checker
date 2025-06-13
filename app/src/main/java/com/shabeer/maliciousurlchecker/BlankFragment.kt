1168f9b60bf9f905d226d82004eccec341b604e8
shaikshabeer <shabeer.shaik@rampgroup.com>
2025-06-13 14:59:44 +0530

    second committed

--- app/src/main/java/com/shabeer/maliciousurlchecker/BlankFragment.kt
+++ app/src/main/java/com/shabeer/maliciousurlchecker/BlankFragment.kt
@@ -24,6 +24,8 @@ class BlankFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
+
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
